feb55b124feac39042399fcd47758d610cd61f75
James <jamesbakker0330@gmail.com>
2023-04-28 18:59:45 +0530

    Link api to database

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+// import "reflect-metadata";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: ".env.local",
+      }
+      )],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('host'),
+        port: +configService.get('port'),
+        username: configService.get('username'),
+        password: configService.get('password'),
+        database: configService.get('database'),
+        synchronize: configService.get<boolean>('sync'),
+      }),
+      inject: [ConfigService],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
