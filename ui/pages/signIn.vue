f43ecfa606cef5baaacf2127149da45e2ac3d3ce
James <jamesbakker0330@gmail.com>
2023-04-28 12:55:20 +0530

    First commit

---
+++ ui/pages/signIn.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <h1>Sign In</h1>
+    <form action="" method="get">
+      <label for="email">Email: </label>
+      <input type="text" id="email">
+      <label for="password">Password: </label>
+      <input type="password" id="password">
+    </form>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ ui/pages/signUp.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Sign Up
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
