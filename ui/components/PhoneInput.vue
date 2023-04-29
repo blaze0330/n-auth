a6e5bea51a98ae1b534acdbffc69dd06f421e774
James <jamesbakker0330@gmail.com>
2023-04-29 23:46:31 +0530

    Update PhoneInput type

--- ui/components/PhoneInput.vue
+++ ui/components/PhoneInput.vue
@@ -3,7 +3,7 @@
     <select name ="country-list" class="focus:outline-slate-500 border-[#DCDEE5] border-[1px] rounded-[4px] h-[48px] w-[88px] relative top-3 mr-2 pl-4" >
       <option v-for="country in allCountries" :key="country" :value="country.dial_code">{{ country.emoji }}</option>
     </select>
-    <input type="number" name="phone" id="phone" class="w-[100%] mt-3 border-[#DCDEE5] border-[1px] bg-[#FFFFFF] h-[48px] rounded-[4px] px-[12px] py-[16px] focus:outline-slate-500" placeholder="Mobile Number">
+    <input type="tel" name="phone" id="phone" class="w-[100%] mt-3 border-[#DCDEE5] border-[1px] bg-[#FFFFFF] h-[48px] rounded-[4px] px-[12px] py-[16px] focus:outline-slate-500" placeholder="Mobile Number">
   </div>
 </template>
 
