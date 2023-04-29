fa43d92e3e11973ad4bc66c750eb8dcc48a933f6
James <jamesbakker0330@gmail.com>
2023-04-29 13:11:20 +0530

    Add welcome banner

--- ui/components/WelcomeBanner.vue
+++ ui/components/WelcomeBanner.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>
+  <div class="hidden md:flex bg-[#404555] w-[40%]">
+    <!-- Left part of the welcome banner the one with the avatar. -->
+    <div class="h-full w-[30%] flex-col-reverse">
+      <img class="w-[165px] h-[219px]" src="./../assets/Avatar.svg" />
+    </div>
+    <!-- Right part of the welcome banner. The one with logo. -->
+    <div class="h-full w-[70%] flex-col justify-around mt-40">
+      <img src="./../assets/Logo.svg" class="w-[8rem] h-[8rem] lg:w-[217px] lg:h-[153px]" alt="">
+      <div>
+        <h3 class="leading-8 text-[#FFFFFF] font-['Poppins'] font-bold">Welcome Back!</h3>
+        <div>
+          Sign In to find opportunities that match your interests. We have both part-time and full-time roles that can be done online and in-person.
+        </div>
+        <!-- Slider dots -->
+        <div class="flex">
+          <span class="block h-[5px] w-[25px] rounded-full bg-white ml-1"></span>
+          <span class="block h-[5px] w-[5px] rounded-full bg-slate-300 ml-1"></span>
+          <span class="block h-[5px] w-[5px] rounded-full bg-slate-300 ml-1"></span>
+        </div>
+        <div>
+          Please contact us at +91-9380644532 if you need any assistance.
 
+        </div>
+      </div>
+    </div>
+    
   </div>
 </template>
 
-<script setup>
+<script>
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
